a3e70074da6afc8993e845efab3cd67d6fffe50e
ilkka.jaaskelainen@tietoevry.com <ilkka.jaaskelainen@tietoevry.com>
2022-04-12 14:26:02 +0300

    removed obsolete comments

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,16 +1,9 @@
-# clean previous output files
-#rm -f robotframework-reports/output.xml
-#rm -f robotframework-reports/rerun.xml
-#rm -f robotframework-reports/first_run_log.html
-#rm -f robotframework-reports/second_run_log.html
-
 echo
 echo "#######################################"
 echo "# Running portfolio a first time      #"
 echo "#######################################"
 echo
 pabot --testlevelsplit --ordering ./environments/helfi_pabot_order_ci --processes 9 -v PREFIX:/kaupunkiymparisto-ja-liikenne -v BASE_URL:varnish-helfi-kymp.docker.so -v PICCOMPARE:False -v useoriginalname:False -v images_dir:robotframework-resources/screenshots/headlesschrome -v actual_dir:robotframework-reports -A ./environments/ci.args -d robotframework-reports . $@
-#robot --outputdir robotframework-reports $@
 
 # we stop the script here if all the tests were OK
 if [ $? -eq 0 ]; then
@@ -28,7 +21,6 @@ echo "#######################################"
 echo "# Running again the tests that failed #"
 echo "#######################################"
 echo
-#robot --outputdir robotframework-reports --nostatusrc --rerunfailed robotframework-reports/output.xml --output rerun.xml $@
 pabot --processes 9 -v PREFIX:/kaupunkiymparisto-ja-liikenne -v BASE_URL:varnish-helfi-kymp.docker.so -v PICCOMPARE:False -v useoriginalname:False -v images_dir:robotframework-resources/screenshots/headlesschrome -v actual_dir:robotframework-reports -A ./environments/ci.args --nostatusrc --rerunfailed robotframework-reports/output.xml --output rerun.xml -d robotframework-reports . $@
 # => Robot Framework generates file rerun.xml
 
