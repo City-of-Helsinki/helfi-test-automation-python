94e98c5e282406c16aeaa270f682b470efeb87be
ilkka.jaaskelainen@tietoevry.com <ilkka.jaaskelainen@tietoevry.com>
2022-04-27 10:33:50 +0300

    changes

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,14 +1,23 @@
+set -e
+if [[ ! -n "$PREFIX" ]]; then
+  PREFIX="/kaupunkiymparisto-ja-liikenne"
+fi
+
+if [[ ! -n "$BASE_URL" ]]; then
+  BASE_URL="varnish-helfi-kymp.docker.so"
+fi
+
 echo
 echo "#######################################"
 echo "# Running portfolio a first time      #"
 echo "#######################################"
 echo
-pabot --testlevelsplit --ordering ./environments/helfi_pabot_order_ci --processes 9 -v PREFIX:/kaupunkiymparisto-ja-liikenne -v BASE_URL:varnish-helfi-kymp.docker.so -v PICCOMPARE:False -v useoriginalname:False -v images_dir:robotframework-resources/screenshots/headlesschrome -v actual_dir:robotframework-reports -A ./environments/ci.args -d robotframework-reports . $@
+pabot --testlevelsplit --ordering ./environments/helfi_pabot_order_ci --processes 9 -v PREFIX:$PREFIX -v BASE_URL:$BASE_URL -v PICCOMPARE:False -v useoriginalname:False -v images_dir:robotframework-resources/screenshots/headlesschrome -v actual_dir:robotframework-reports -A ./environments/ci.args -d robotframework-reports . $@
 
 # we stop the script here if all the tests were OK
 if [ $? -eq 0 ]; then
 	echo "we don't run the tests again as everything was OK on first try"
-	exit 0	
+	exit 0
 fi
 # otherwise we go for another round with the failing tests
 
@@ -21,7 +30,7 @@ echo "#######################################"
 echo "# Running again the tests that failed #"
 echo "#######################################"
 echo
-pabot --processes 9 -v PREFIX:/kaupunkiymparisto-ja-liikenne -v BASE_URL:varnish-helfi-kymp.docker.so -v PICCOMPARE:False -v useoriginalname:False -v images_dir:robotframework-resources/screenshots/headlesschrome -v actual_dir:robotframework-reports -A ./environments/ci.args --nostatusrc --rerunfailed robotframework-reports/output.xml --output rerun.xml -d robotframework-reports . $@
+pabot --processes 9 -v PREFIX:$PREFIX -v BASE_URL:$BASE_URL -v PICCOMPARE:False -v useoriginalname:False -v images_dir:robotframework-resources/screenshots/headlesschrome -v actual_dir:robotframework-reports -A ./environments/ci.args --nostatusrc --rerunfailed robotframework-reports/output.xml --output rerun.xml -d robotframework-reports . $@
 # => Robot Framework generates file rerun.xml
 
 # we keep a copy of the second log file
