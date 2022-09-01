ddff717234091958359b0a1f3d2c896a71ff6a65
jaaskilktevry <78489263+jaaskilktevry@users.noreply.github.com>
2022-09-01 12:57:56 +0300

    added etusivu_spesific tag exclude

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -11,7 +11,7 @@ echo "#######################################"
 echo "# Running portfolio a first time      #"
 echo "#######################################"
 echo
-pabot --testlevelsplit --ordering ./environments/helfi_pabot_order_ci --processes 9 -v PREFIX:$PREFIX -v BASE_URL:$BASE_URL -v PICCOMPARE:False -v useoriginalname:False -v images_dir:robotframework-resources/screenshots/headlesschrome -v actual_dir:robotframework-reports -A ./environments/ci.args -d robotframework-reports . $@
+pabot --testlevelsplit --ordering ./environments/helfi_pabot_order_ci --processes 9 -e ETUSIVU_SPESIFIC -v PREFIX:$PREFIX -v BASE_URL:$BASE_URL -v PICCOMPARE:False -v useoriginalname:False -v images_dir:robotframework-resources/screenshots/headlesschrome -v actual_dir:robotframework-reports -A ./environments/ci.args -d robotframework-reports . $@
 EXIT_CODE=$?
 
 # we stop the script here if all the tests were OK
@@ -30,7 +30,7 @@ echo "#######################################"
 echo "# Running again the tests that failed #"
 echo "#######################################"
 echo
-pabot --processes 9 -v PREFIX:$PREFIX -v BASE_URL:$BASE_URL -v PICCOMPARE:False -v useoriginalname:False -v images_dir:robotframework-resources/screenshots/headlesschrome -v actual_dir:robotframework-reports -A ./environments/ci.args --rerunfailed robotframework-reports/output.xml --output rerun.xml -d robotframework-reports . $@
+pabot --processes 9 -e ETUSIVU_SPESIFIC -v PREFIX:$PREFIX -v BASE_URL:$BASE_URL -v PICCOMPARE:False -v useoriginalname:False -v images_dir:robotframework-resources/screenshots/headlesschrome -v actual_dir:robotframework-reports -A ./environments/ci.args --rerunfailed robotframework-reports/output.xml --output rerun.xml -d robotframework-reports . $@
 EXIT_CODE2=$?
 
 # => Robot Framework generates file rerun.xml
